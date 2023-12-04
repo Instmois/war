8cca077d52f21fc2974ac7504508df6ced4d406c
Instmois <nikita-moiseev-2003@ya.ru>
2023-12-04 22:08:16 +0300

    dead end

--- public/client.js
+++ public/client.js
@@ -26,14 +26,11 @@ document.addEventListener('DOMContentLoaded', function () {
       
     });
 
-    socket.on('gameStarted', ({ userId, games }) => {
+    socket.on('gameStarted', ({ userId, games, GameBoard }) => {
       console.log('Game started!');
       console.log('User ID:', userId);
       console.log('Hiding buttons and showing game board...');
-      createGameBtn.style.display = 'none';
-      joinBtn.style.display = 'none';
-      gameBoard.style.display = 'block';
-      showGameBoard();
+      showGameBoard(games);
     });
 
     socket.on('gameNotFound', () => {
@@ -42,10 +39,65 @@ document.addEventListener('DOMContentLoaded', function () {
     socket.on('notTwoPeople', () => {
       alert('Please, waiting for player 2');
     });
-    function showGameBoard() {
-      // Реализуйте логику для отображения игрового поля (например, скрытие кнопок и показ игрового поля)
+    socket.on('CellNotAvailable', () => {
+      alert('Выбранная клетка недоступна. Попробуйте другую.');
+    });
+   
+
+    socket.on('updateBoard', ({ row, col, symbol }) => {
+      // Обновляем интерфейс на основе данных о ходе от сервера
+      const cellElement = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+      
+      if (symbol === 'cross') {
+        cellElement.innerHTML = 'X'; // Ваш символ для крестика
+      } else {
+        cellElement.innerHTML = 'O'; // Ваш символ для нолика
+      }
+    });
+
+    function showGameBoard(games) {
+      createGameBtn.style.display = 'none';
+      joinBtn.style.display = 'none';
+      gameBoard.style.display = 'grid';
+      
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          const cell = document.createElement('div');
+          cell.className = 'cell';
+          cell.setAttribute('data-row', i);
+          cell.setAttribute('data-col', j);
+          cell.addEventListener('click', () => onCellClick(i, j));
+          gameBoard.appendChild(cell);
+        }
+      }
+      
       console.log('Game board displayed. Waiting for Player 2...');
+    
+      function onCellClick(row, col) {
+        const symbol = (games[0].players[0] === socket.id) ? 'cross' : 'circle';
+    
+        // Отправляем информацию на сервер о намерении сделать ход
+        socket.emit('makeMoveRequest', { row, col, symbol });
+      }
     }
+    /*
+      function onCellClick(row, col, GameBoard) {
+        const symbol = (games[0].players[0] === socket.id) ? 'cross' : 'circle';
+  
+        if (GameBoard.isCellAvailable(row, col, symbol)) {
+          // Если клетка доступна, делаем ход
+          GameBoard.makeMove(row, col, symbol);
+          GameBoard.printBoard();
+          // Дополнительная логика для отправки информации на сервер
+          socket.emit('makeMove', { row, col, symbol });
+          
+        } else {
+          alert('Выбранная клетка недоступна. Попробуйте другую.');
+        }
+        console.log(`Нажатие на ячейку (${row}, ${col})`);
+      }
+    }*/
+
   })();
 });
 
--- server.js
+++ server.js
@@ -12,6 +12,77 @@ app.use(express.static('public'));
 
 let games = [];
 let users = [];
+const MAX_MOVES = 3;
+let removePlayer = false;
+const GameBoard = {
+  cells: [],
+  movesCount: {
+    cross: 0,
+    circle: 0,
+  },
+
+  initBoard: function () {
+    for (let i = 0; i < 10; i++) {
+      this.cells[i] = [];
+      for (let j = 0; j < 10; j++) {
+        this.cells[i][j] = 'empty';
+      }
+    }
+    this.cells[0][0] = 'cross';
+    this.cells[9][9] = 'circle';
+  },
+
+  isCellAvailable: function (row, col, symbol) {
+    // Проверяем, доступна ли клетка для заданного символа
+    if (symbol === 'cross' && this.cells[row][col] === 'empty') {
+      return (
+        (row > 0 && this.cells[row - 1][col] === 'cross') ||
+        (col > 0 && this.cells[row][col - 1] === 'cross') ||
+        (row < 9 && this.cells[row + 1][col] === 'cross') ||
+        (col < 9 && this.cells[row][col + 1] === 'cross') ||
+        (row > 0 && col > 0 && this.cells[row - 1][col - 1] === 'cross') ||
+        (row > 0 && col < 9 && this.cells[row - 1][col + 1] === 'cross') ||
+        (row < 9 && col > 0 && this.cells[row + 1][col - 1] === 'cross') ||
+        (row < 9 && col < 9 && this.cells[row + 1][col + 1] === 'cross')
+      );
+    } else if (symbol === 'circle' && this.cells[row][col] === 'empty') {
+      return (
+        (row > 0 && this.cells[row - 1][col] === 'circle') ||
+        (col > 0 && this.cells[row][col - 1] === 'circle') ||
+        (row < 9 && this.cells[row + 1][col] === 'circle') ||
+        (col < 9 && this.cells[row][col + 1] === 'circle') ||
+        (row > 0 && col > 0 && this.cells[row - 1][col - 1] === 'circle') ||
+        (row > 0 && col < 9 && this.cells[row - 1][col + 1] === 'circle') ||
+        (row < 9 && col > 0 && this.cells[row + 1][col - 1] === 'circle') ||
+        (row < 9 && col < 9 && this.cells[row + 1][col + 1] === 'circle')
+      );
+    }
+    return true;
+  },
+
+  makeMove: function (row, col, symbol) {
+    // Выполняем ход (ставим символ или убиваем чужой символ)
+    this.cells[row][col] = symbol;
+    this.movesCount[symbol]++;
+  },
+  checkWinCondition: function () {
+    // Реализуйте логику условия победы здесь
+  },
+
+  checkDrawCondition: function () {
+    // Реализуйте логику условия ничьи здесь
+  },
+  printBoard: function () {
+    // Выводим текущее состояние игрового поля в консоль
+    console.log(this.cells);
+    for (let i = 0; i < 10; i++) {
+      for(let j = 0; j < 10; j++){
+        //console.log(this.cells[i].join(' '));
+      }
+      
+    }
+  },
+};
 
 io.on('connection', (socket) => {
   const userId = socket.id; // уникальный идентификатор пользователя
@@ -20,7 +91,7 @@ io.on('connection', (socket) => {
   console.log('A user connected :' + userId);
   socket.on('createGame', ({userId}) => {
     const gameId = (Math.floor(Math.random() * (1000 - 1)) + 1);
-    games.push({ id: gameId, players: [userId] });
+    games.push({ id: gameId, players: [userId], activePlayer: 'cross' });
     socket.join(gameId);
     console.log(games);
     socket.emit('gameCreated', gameId);
@@ -30,25 +101,69 @@ io.on('connection', (socket) => {
   console.log('Join game request from user:', userId);
   console.log('Current games:', games);
   console.log('Game ID to join:', gameId);
-  console.log('Does the game ID match?', games && games.some(game => game.id == gameId));
     if (users.length == 2) {
         if (games && (games[0].id == gameId)) {
             games[0].players.push(userId);  // Обращаемся к массиву players внутри объекта game
             socket.join(gameId);
-            io.emit('gameStarted', { userId, games });
+            GameBoard.initBoard();
+            GameBoard.printBoard();
+            io.emit('gameStarted', { userId, games, GameBoard });
         } else {
             socket.emit('gameNotFound');
         }
     } else {
         socket.emit('notTwoPeople');
     }
-});
+  });
+  socket.on('makeMove', ({ row, col, symbol }) => {
+    // Обработка хода и отправка обновлений всем участникам игры
+    io.emit('updateBoard', { row, col, symbol });
+  });
+  let isMoveExecuted = false;
+  socket.on('makeMoveRequest', ({ row, col, symbol }) => {
+    const currentGame = games[0];
+    const currentPlayerMoves = GameBoard.movesCount[symbol];
+    console.log(currentPlayerMoves);
+    console.log(currentGame.activePlayer);
+    console.log(currentPlayerMoves < MAX_MOVES && symbol === currentGame.activePlayer);
+    console.log(GameBoard.isCellAvailable(row, col, symbol));
+    if (currentPlayerMoves < MAX_MOVES && symbol == currentGame.activePlayer){
 
+      if (GameBoard.isCellAvailable(row, col, symbol)) {
 
+        GameBoard.makeMove(row, col, symbol);
+        if (!isMoveExecuted) {
+        console.log('До увеличения:', GameBoard.movesCount[symbol]);
+
+        GameBoard.movesCount[symbol] = GameBoard.movesCount[symbol] + 1;
+
+        console.log('После увеличения:', GameBoard.movesCount[symbol]);
+        // Отправляем информацию об обновлении всем участникам игры
+        console.log(currentPlayerMoves);
+        io.emit('updateBoard', { row, col, symbol });
+        // Дополнительная логика для обработки победы или ничьи, если нужно
+        //if (GameBoard.checkWinCondition()) {
+        // io.to(gameId).emit('gameWon', { winner: socket.id });
+        //} else if (GameBoard.checkDrawCondition()) {
+        // io.to(gameId).emit('gameDraw');
+        //}
+        if ( GameBoard.movesCount[symbol] == MAX_MOVES) {
+          // Меняем активного игрока
+          games[0].activePlayer = symbol === 'cross' ? 'circle' : 'cross';
+          io.emit('changePlayerTurn', { nextPlayer: games[0].activePlayer });
+          GameBoard.movesCount[symbol] = 0;
+        }
+        isMoveExecuted = true;
+      }} else {
+        socket.emit('CellNotAvailable');
+      }
+    } else {
+      socket.emit('endMovePlayer');
+    }
+  });
   socket.on('disconnect', () => {
     console.log('A user disconnected:' + userId);
     users = users.filter((user) => user !== userId);
-    // Implement logic to handle user disconnection and game termination
   });
 });
 
